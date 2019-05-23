2d82c4972ecda079cdcd6fc9f7dfdfe4f72b7bdf
James Luo <12567921+jluo117@users.noreply.github.com>
2019-05-23 10:45:00 -0700

    fix pins

--- TempReader/TempReader/main.c
+++ TempReader/TempReader/main.c
@@ -13,35 +13,39 @@
 uint8_t c=0,I_RH,D_RH,I_Temp,D_Temp,CheckSum;
 void Request()				/* Microcontroller send start pulse/request */
 {
-	DDRD |= (1<<DHT11_PIN);
-	PORTD &= ~(1<<DHT11_PIN);	/* set to low pin */
+	DDRA |= (1<<DHT11_PIN);
+	PORTA &= ~(1<<DHT11_PIN);	/* set to low pin */
 	_delay_ms(20);			/* wait for 20ms */
-	PORTD |= (1<<DHT11_PIN);	/* set to high pin */
+	PORTA |= (1<<DHT11_PIN);	/* set to high pin */
 }
 void Response()				/* receive response from DHT11 */
 {
-	DDRD &= ~(1<<DHT11_PIN);
-	while(PIND & (1<<DHT11_PIN));
-	while((PIND & (1<<DHT11_PIN))==0);
-	while(PIND & (1<<DHT11_PIN));
+	DDRA &= ~(1<<DHT11_PIN);
+	while(PINA & (1<<DHT11_PIN));
+	while((PINA & (1<<DHT11_PIN))==0);
+	while(PINA & (1<<DHT11_PIN));
 }
 uint8_t Receive_data()			/* receive data */
 {
 	for (int q=0; q<8; q++)
 	{
-		while((PIND & (1<<DHT11_PIN)) == 0);  /* check received bit 0 or 1 */
+		while((PINA & (1<<DHT11_PIN)) == 0);  /* check received bit 0 or 1 */
 		_delay_us(30);
-		if(PIND & (1<<DHT11_PIN))/* if high pulse is greater than 30ms */
+		if(PINA & (1<<DHT11_PIN))/* if high pulse is greater than 30ms */
 		c = (c<<1)|(0x01);	/* then its logic HIGH */
 		else			/* otherwise its logic LOW */
 		c = (c<<1);
-		while(PIND & (1<<DHT11_PIN));
+		while(PINA & (1<<DHT11_PIN));
 	}
 	return c;
 }
 
 int main(void)
 {
+	DDRA = 0x00;
+	PORTA = 0xFF;
+	DDRB = 0xFF;
+	PORTB = 0x00;
     /* Replace with your application code */
     while (1) 
     {
@@ -52,6 +56,7 @@ int main(void)
 		I_Temp=Receive_data();	/* store next eight bit in I_Temp */
 		D_Temp=Receive_data();	/* store next eight bit in D_Temp */
 		CheckSum=Receive_data();/* store next eight bit in CheckSum */
+		PORTB = I_Temp;
     }
 }
 
