36f8150ef4633852b9e0eaa532c730e963583c77
James Luo <12567921+jluo117@users.noreply.github.com>
2019-05-23 10:13:18 -0700

    copy code

---
+++ TempReader/TempReader/main.c
@@ -0,0 +1,57 @@
+/*
+ * TempReader.c
+ *
+ * Created: 5/23/2019 10:00:59 AM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <util/delay.h>
+#define DHT11_PIN 6
+uint8_t c=0,I_RH,D_RH,I_Temp,D_Temp,CheckSum;
+void Request()				/* Microcontroller send start pulse/request */
+{
+	DDRD |= (1<<DHT11_PIN);
+	PORTD &= ~(1<<DHT11_PIN);	/* set to low pin */
+	_delay_ms(20);			/* wait for 20ms */
+	PORTD |= (1<<DHT11_PIN);	/* set to high pin */
+}
+void Response()				/* receive response from DHT11 */
+{
+	DDRD &= ~(1<<DHT11_PIN);
+	while(PIND & (1<<DHT11_PIN));
+	while((PIND & (1<<DHT11_PIN))==0);
+	while(PIND & (1<<DHT11_PIN));
+}
+uint8_t Receive_data()			/* receive data */
+{
+	for (int q=0; q<8; q++)
+	{
+		while((PIND & (1<<DHT11_PIN)) == 0);  /* check received bit 0 or 1 */
+		_delay_us(30);
+		if(PIND & (1<<DHT11_PIN))/* if high pulse is greater than 30ms */
+		c = (c<<1)|(0x01);	/* then its logic HIGH */
+		else			/* otherwise its logic LOW */
+		c = (c<<1);
+		while(PIND & (1<<DHT11_PIN));
+	}
+	return c;
+}
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		Request();		/* send start pulse */
+		Response();		/* receive response */
+		I_RH=Receive_data();	/* store first eight bit in I_RH */
+		D_RH=Receive_data();	/* store next eight bit in D_RH */
+		I_Temp=Receive_data();	/* store next eight bit in I_Temp */
+		D_Temp=Receive_data();	/* store next eight bit in D_Temp */
+		CheckSum=Receive_data();/* store next eight bit in CheckSum */
+    }
+}
+
